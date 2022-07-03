e3f6d66151ebb499c456f61df7ad0cfceb5e5223
Anishchenko Stanislav <anischenko_stas@mail.ru>
2022-07-03 14:42:08 +0500

    finished

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HealthBar : Bar
+{
+    [SerializeField] private Player _player;
+
+    private void OnEnable()
+    {
+        _player.HealthChanged += OnValueChange;
+        Slider.value = 1;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthChanged -= OnValueChange;
+    }
+}
+
