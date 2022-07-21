334466c66d2fa9fe8254ab472647268d3190e170
Anishchenko Stanislav <anischenko_stas@mail.ru>
2022-07-21 18:10:12 +0500

    Removed extra lines. Added Uzi.cs and Uzi.prefab

---
+++ Assets/Scripts/Weapons/Uzi.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+
+public class Uzi : Weapon
+{
+    private Coroutine _coroutine;
+
+    public override void Shoot(Transform shootPosition)
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(DoShoot(shootPosition));
+    }
+
+    private IEnumerator DoShoot(Transform shootPosition)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(Bullet, shootPosition.position, Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
